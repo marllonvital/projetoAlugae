9228bcf0a74c0a713ac66d1925e0a43427e6e88e
FabioRNobrega <fabio.r.nobrega@gmail.com>
2019-02-11 16:14:53 -0200

    +iniciel page Home | + Adicionei fonts, favicon, e css global para h1 h2 h3 h4 h5 h6

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import{HomeComponent} from './pages/home/home.component';
+
+const routes: Routes = [
+
+    { path: 'home', component: HomeComponent},
+    {path: '',redirectTo: '/home',pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
