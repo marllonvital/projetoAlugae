137aa4c13ee6363d2927c142b57e0638798a23b4
Marllon <marllonvital@dcc.ufrj.br>
2019-02-12 15:00:27 -0200

    # erros na bank migration

--- Alugae/database/migrations/2019_02_11_173335_create_bank_informations_table.php
+++ Alugae/database/migrations/2019_02_11_173335_create_bank_informations_table.php
@@ -7,25 +7,24 @@ use Illuminate\Database\Migrations\Migration;
 class CreateBankInformationsTable extends Migration
 {
 
-    public function up()
-    {
-        Schema::create('bank_informations', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('numero_do_cartao');
-            $table->string('tipo_do_cartao');
-            $table->string('validade');
-            $table->string('bandeira');
-            $table->integer('usuario_id')->unsigned();
-            $table->timestamps();
-        });
+public function up()
+{
+    Schema::create('bank_informations', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('numero_do_cartao');
+        $table->string('validade');
+        $table->string('tipo_do_cartao');
+        $table->string('bandeira');
+        $table->integer('usuario_id')->unsigned()->nullable();
+        $table->timestamps();
+    });
 
-        Schema::table('bank_informations', function (Blueprint $table) {
-            $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
-        });
+    Schema::table('bank_informations', function (Blueprint $table) {
+        $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
+    });
     }
 
     public function down()
     {
         Schema::dropIfExists('bank_informations');
     }
-}
