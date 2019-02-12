62deb7d7b2e9995d03e7cf7c0023ec56aa47e13d
maycon <maycon@dcc.ufrj.br>
2019-02-12 21:05:11 -0200

    models + controllers final

--- Alugae/database/migrations/2019_02_11_181653_create_bank_informations_table.php
+++ Alugae/database/migrations/2019_02_11_181653_create_bank_informations_table.php
@@ -22,7 +22,38 @@ class CreateBankInformationsTable extends Migration
         Schema::table('bank_informations', function (Blueprint $table) {
             $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
         });
+    }<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBankInformationsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('bank_informations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('numero_do_cartao');
+            $table->string('validade');
+            $table->string('tipo_do_cartao');
+            $table->string('bandeira');
+            $table->integer('usuario_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('bank_informations', function (Blueprint $table) {
+            $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
+        });
     }
+
+    public function down()
+    {
+        Schema::dropIfExists('bank_informations');
+    }
+}
+
 
     public function down()
     {
