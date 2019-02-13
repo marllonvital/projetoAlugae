e244cfe1591a86556d606c0fac328218d67b23e4
FabioRNobrega <fabio.r.nobrega@gmail.com>
2019-02-13 15:40:30 -0200

    criei página Sobre

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import{HomeComponent} from './pages/home/home.component';
+import{SobreComponent} from './pages/sobre/sobre.component';
 import{ProdutosComponent} from './components/produtos/produtos.component';
 
 const routes: Routes = [
 
     { path: 'home', component: HomeComponent},
+    {path: 'sobre', component: SobreComponent},
     {path: '',redirectTo: '/home',pathMatch: 'full'}
 ];
 
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 
 
@@ -15,6 +16,7 @@ import { ProdutosComponent } from './components/produtos/produtos.component';
     AppComponent,
     HomeComponent,
     ProdutosComponent,
+    SobreComponent,
   ],
   imports: [
     BrowserModule,
