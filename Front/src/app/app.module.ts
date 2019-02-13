f4792dbef35abe046310987b97c284f680b49e3e
Gabriel Conde <gabriel.wave.conde@gmail.com>
2019-02-13 15:39:42 -0200

    + adicionando o footer | *modificando o posicionamento da lupa e da busca desktop na navbar

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { MaterializeModule } from 'angular2-materialize';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
