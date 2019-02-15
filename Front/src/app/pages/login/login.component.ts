2f581d9f669b3909972a87198775801b1ec8b4f4
FabioRNobrega <fabio.r.nobrega@gmail.com>
2019-02-15 19:29:57 -0200

    +Ativei botão de sobre|criar conta |login

--- Front/src/app/pages/login/login.component.ts
+++ Front/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from './service/login.service'
+import { LoginService } from '../../service/login.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
