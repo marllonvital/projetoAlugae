3ffd1f22183075c75422819f710f080c3dc1f23d
Gabriel Conde <gabriel.wave.conde@gmail.com>
2019-02-17 00:37:38 -0300

    *carrossel funcionando

--- Front/src/app/components/produtos/produtos.component.ts
+++ Front/src/app/components/produtos/produtos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, ViewChild} from "@angular/core"
+import {MaterializeAction} from "angular2-materialize"
 
 @Component({
   selector: 'app-produtos',
@@ -6,28 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
+
+  @ViewChild('carousel') carouselElement;
+ actions = new EventEmitter<string>();
+
   produto: any[]=[
     {nome: "Super Nintendo ",
      preco: 100,
      ul: "assets/img/produto_defaut/defaut.png"}
   ];
-  constructor() { }
+
+  imageURLs = [
+  "https://image.shutterstock.com/display_pic_with_logo/1264645/364153082/stock-photo-asian-girl-in-sunflower-field-364153082.jpg",
+  "https://image.shutterstock.com/display_pic_with_logo/1264645/298927574/stock-photo-a-young-traveler-girl-sit-on-the-wooden-bridge-in-halong-bay-and-enjoy-the-beauty-of-seascape-298927574.jpg",
+  "https://image.shutterstock.com/display_pic_with_logo/1264645/298757792/stock-photo-a-young-traveler-girl-sit-on-the-top-of-mountain-in-halong-bay-and-enjoy-the-beauty-of-seascape-298757792.jpg",
+  "https://image.shutterstock.com/display_pic_with_logo/2565601/411902890/stock-photo-ha-long-bay-scenic-view-hanoi-vietnam-411902890.jpg",
+  "https://image.shutterstock.com/display_pic_with_logo/2565601/413207668/stock-photo-the-temple-of-literature-in-hanoi-vietnam-the-chinese-words-is-poem-of-thie-temple-and-templs-s-413207668.jpg",
+  "https://picsum.photos/1000/?random",
+  "https://picsum.photos/1000/?random",
+  "https://picsum.photos/1000/?random",
+  "https://picsum.photos/1000/?random",
+
+];
+  // constructor() { }
+
+   showInitialized = false;
+
+  constructor() {
+  // example of a hacky way to add an image to the carousel dynamically
+  window.setTimeout(() => {
+    this.imageURLs = [this.imageURLs[1], ...this.imageURLs]; // duplicate the first iamge
+    this.carouselElement.nativeElement.classList.toggle("initialized")
+    this.actions.emit("carousel");
+  },1000);
+}
 
 
   ngOnInit() {
   }
 
- //  $('.moveNextCarousel').click(function(e){
- //    e.preventDefault();
- //    e.stopPropagation();
- //    $('.carousel').carousel('next');
- // });
- //
- // // move prev carousel
- // $('.movePrevCarousel').click(function(e){
- //    e.preventDefault();
- //    e.stopPropagation();
- //    $('.carousel').carousel('prev');
- // });
+  moveNextCarousel(){
+    this.actions.emit({ action: 'carousel', params: ['next'] });
+ }
+
+ // move prev carousel
+ movePrevCarousel(){
+   this.actions.emit({ action: 'carousel', params: ['prev'] });
+
+ }
 
 }
