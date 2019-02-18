33d0f877d51387e378a3eaf503dc7b1497ee908f
FabioRNobrega <fabio.r.nobrega@gmail.com>
2019-02-18 15:18:13 -0300

    Segundo teste de Integração

--- Front/src/app/service/cadastro-produto.service.ts
+++ Front/src/app/service/cadastro-produto.service.ts
@@ -9,7 +9,7 @@ import { Observable} from 'rxjs';
 })
 export class CadastroProdutoService {
 
-  apiUrl = "http://localhot:8000/api/product-store";
+  apiUrl = "http://localhot:8000/api/product-add";
 
 
 
